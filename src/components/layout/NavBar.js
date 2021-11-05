b5394d8ce918bc80b21c928517b6a7b693396206
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 14:50:18 -0300

    Add past and file Pokemon

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -7,8 +7,11 @@ export default class NavBar extends Component {
         return (
             <div>
                <nav 
+               href=""
                className="navbar navbar-expand-md navbar-dark  fixed-top" >
-                   <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">Pokedex</a>
+                   <a className="navbar-brand col-sm-3 col-md-2 mr-0 align-items-center">
+                       Pokedex
+                       </a>
                    </nav>
             </div>
         );
