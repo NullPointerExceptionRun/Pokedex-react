82ea0ab2041769cbb70cbc7909c247328293d5ba
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 14:57:02 -0300

    pokemon list e card alterados

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class PokemonCard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="col-md-3 col-sm-6 mb-5">
+        {/* Cards pokemon */}
+        <div className="card">
+          <div className="card-header">
+              <h1>herucles snatos</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
