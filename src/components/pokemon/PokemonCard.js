5bae514eebc1b6801bb4ae43845adf61e829fcac
herculesdevbr <herculessantos306@gmail.com>
2021-11-06 01:40:08 -0300

    working in pokemon Card

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -7,17 +7,19 @@ export default class PokemonCard extends Component {
     pokemonIndex: '',
   };
 
-  render() {
+  componentDidMount() {
     const {name, url} = this.props;
- 
-  
-    return (
+    const pokemonIndex = url.split('/')[url.split('/').length - 2];
+  }
+
+    render() {
+    return  (
       <div className="col-md-3 col-sm-6 mb-5">
         {/* Cards pokemon */}
         <div className="card">
           <div className="card-header">
             
-            <h1>{name}</h1>
+            <h1>{this.state.name}</h1>
           </div>
         </div>
       </div>
