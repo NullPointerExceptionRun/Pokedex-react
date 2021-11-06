66925861218b24855e5041d289218c4bcbf2de89
herculesdevbr <herculessantos306@gmail.com>
2021-11-06 01:00:58 -0300

    Add props

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
 
 export default class PokemonCard extends Component {
-state ={
-  name: '',
-  imageUrl: '',
-  pokemonIndex: '',
+  state = {
+    name: "",
+    imageUrl: "",
+    pokemonIndex: "",
+  };
 
-};
-
-
-
+  render() {
+    const name = this.props.name;
+    const url = this.props.url;
+  }
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         {/* Cards pokemon */}
         <div className="card">
           <div className="card-header">
-              <h1></h1>
+            <h1></h1>
           </div>
         </div>
       </div>
