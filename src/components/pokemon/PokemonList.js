ef83f4d3190d19d074529e4d906c03c66443d5b6
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 21:51:36 -0300

    incluindo mais informações para consumo da API

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -4,7 +4,12 @@ import PokemonCard from './PokemonCard';
 export default class PokemonList extends Component {
     render() {
         return (
-            <div>
+            <div className="row">
+                <PokemonCard  />
+                <PokemonCard  />
+                <PokemonCard  />
+                <PokemonCard  />
+                <PokemonCard  />
                 <PokemonCard  />
             </div>
         )
