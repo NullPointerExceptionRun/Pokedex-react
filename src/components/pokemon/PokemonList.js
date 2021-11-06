b84fe130eed716d0433959c5ba91e9c2bb2fb3e0
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 22:57:10 -0300

    Trabalhando e manipulando os dados

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import PokemonCard from './PokemonCard';
-import axios from 'axios';
+import React, { Component } from "react";
+import PokemonCard from "./PokemonCard";
+import axios from "axios";
 
 export default class PokemonList extends Component {
-    state={
-        url: "https://pokeapi.com/api/v2/pokemon",
-        pokemon: null
-    };
-componentDidMount(){
-        const res = axios.get(this.state.url);
-}
-    render() {
-        return (
-            <div className="row">
-                <PokemonCard  />
-                <PokemonCard  />
-                <PokemonCard  />
-                <PokemonCard  />
-                <PokemonCard  />
-                <PokemonCard  />
-            </div>
-        )
-    }
+  state = {
+    url: "https://pokeapi.com/api/v2/pokemon",
+    pokemon: null,
+  };
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({ pokemon: res.data["results"] });
+  }
+  render() {
+    return (
+        <div>
+        {this.state.pokmeon ? (<div className="row">
+        {this.state.pokemon.map(pokemon => 
+        (<PokemonCard/>
+        ))}
+      </div>) : (<h1>Loading Pokemon</h1>)}
+      </div>
+    );
+  }
 }
