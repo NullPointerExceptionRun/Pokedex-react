9b6f9fa97034291ce0bd34d73358ff8408653479
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 22:46:24 -0300

    add use state, async

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import PokemonCard from './PokemonCard';
+import axios from 'axios';
 
 export default class PokemonList extends Component {
     state={
         url: "https://pokeapi.com/api/v2/pokemon",
         pokemon: null
     };
-
+componentDidMount(){
+        const res = axios.get(this.state.url);
+}
     render() {
         return (
             <div className="row">
