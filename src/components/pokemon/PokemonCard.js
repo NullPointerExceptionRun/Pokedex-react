cb703c2516276dcda090bea6d665c9c774759922
herculesdevbr <herculessantos306@gmail.com>
2021-11-06 00:58:29 -0300

    adicionando info sobre os pokemons

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
 export default class PokemonCard extends Component {
+state ={
+  name: '',
+  imageUrl: '',
+  pokemonIndex: '',
+
+};
+
+
+
   render() {
     return (
       <div className="col-md-3 col-sm-6 mb-5">
         {/* Cards pokemon */}
         <div className="card">
           <div className="card-header">
-              <h1>Tabela 1</h1>
+              <h1></h1>
           </div>
         </div>
       </div>
