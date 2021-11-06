d6c8ba1fb6d93fa9c2e6e4f7f5c745419d7cbfe3
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 21:45:02 -0300

    manipulando elementos com css

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -7,7 +7,7 @@ export default class PokemonCard extends Component {
         {/* Cards pokemon */}
         <div className="card">
           <div className="card-header">
-              <h1>Hercules Pokedex</h1>
+              <h1>Tabela 1</h1>
           </div>
         </div>
       </div>
