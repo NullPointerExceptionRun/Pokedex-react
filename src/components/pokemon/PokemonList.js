34b70930b7dd75c18b9d49dd565e85ed6589b3b6
herculesdevbr <herculessantos306@gmail.com>
2021-11-05 22:29:56 -0300

    Adicionando dados da API

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import PokemonCard from './PokemonCard';
 
 export default class PokemonList extends Component {
+    state={
+        url: "https://pokeapi.com/api/v2/pokemon",
+        pokemon: null
+    };
+
     render() {
         return (
             <div className="row">
